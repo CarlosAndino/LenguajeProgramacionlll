5c84e24984128025c28f47ac417a978f7611ad11
CarlosAndino <86394965+CarlosAndino@users.noreply.github.com>
2021-06-24 03:00:10 -0600

    Add files via upload

---
+++ Ejercicio Numero 4/Ejercicio Numero 4/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Ejercicio_Numero_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cb1 = new System.Windows.Forms.ComboBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.list1 = new System.Windows.Forms.ListBox();
+            this.cb2 = new System.Windows.Forms.ComboBox();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.list2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // cb1
+            // 
+            this.cb1.FormattingEnabled = true;
+            this.cb1.Location = new System.Drawing.Point(364, 48);
+            this.cb1.Name = "cb1";
+            this.cb1.Size = new System.Drawing.Size(69, 21);
+            this.cb1.TabIndex = 0;
+            this.cb1.SelectedIndexChanged += new System.EventHandler(this.cb1_SelectedIndexChanged);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(9, 10);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(124, 32);
+            this.btn1.TabIndex = 3;
+            this.btn1.Text = "Cargar Datos";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nombre Completo :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(320, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Edad :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(334, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Estadiantes Matriculados :";
+            // 
+            // list1
+            // 
+            this.list1.FormattingEnabled = true;
+            this.list1.Location = new System.Drawing.Point(143, 97);
+            this.list1.Name = "list1";
+            this.list1.Size = new System.Drawing.Size(127, 95);
+            this.list1.TabIndex = 8;
+            this.list1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // cb2
+            // 
+            this.cb2.FormattingEnabled = true;
+            this.cb2.Location = new System.Drawing.Point(109, 48);
+            this.cb2.Name = "cb2";
+            this.cb2.Size = new System.Drawing.Size(161, 21);
+            this.cb2.TabIndex = 9;
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(41, 212);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(129, 33);
+            this.btn2.TabIndex = 10;
+            this.btn2.Text = "Agregar Datos";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(250, 212);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(134, 33);
+            this.btn3.TabIndex = 12;
+            this.btn3.Text = "Ordenar Alfabeticamente";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click_1);
+            // 
+            // list2
+            // 
+            this.list2.FormattingEnabled = true;
+            this.list2.Location = new System.Drawing.Point(263, 97);
+            this.list2.Name = "list2";
+            this.list2.Size = new System.Drawing.Size(22, 95);
+            this.list2.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 257);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.list2);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.cb2);
+            this.Controls.Add(this.list1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.cb1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cb1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox list1;
+        private System.Windows.Forms.ComboBox cb2;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.ListBox list2;
+    }
+}
+
---
+++ Ejercicio Numero 4/Ejercicio Numero 4/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Ejercicio_Numero_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            //arreglo nombres
+            string[] nombre = new string[] { "Carlos Andino", "Katherine Marquez", "Valery Marquez", "Rosa Andino", "Angel Espinoza" };
+            Array.Sort(nombre);
+            foreach (string x  in nombre)
+            {
+                cb2.Items.Add(x);
+            }
+
+            int[] edad = new int[] { 13, 12, 14, 16, 15 };
+            Array.Sort(edad);
+            foreach(var item in edad)
+            {
+                cb1.Items.Add(item);
+            }
+             
+
+            
+        }
+
+        private void cb1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+          
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            list1.Items.Add(cb2.SelectedItem);       
+            list2.Items.Add(cb1.SelectedItem);
+
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+           
+        {
+            list1.Sorted = true;
+
+
+        }
+
+        private void btn3_Click_1(object sender, EventArgs e)
+        {
+            list1.Sorted = true;
+            
+        }
+    }
+}
